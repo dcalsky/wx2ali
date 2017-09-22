7dd22e12b2260af0159371656abe56b5db34f665
dcalsky <dcalsky@163.com>
2017-09-23 00:50:36 +0800

    chore(dist): remove dist folder

--- dist/TransformJS.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const AST_1 = require("./AST");
-function transform_js(data) {
-    return AST_1.transform(data);
-}
-exports.transform_js = transform_js;
-//# sourceMappingURL=TransformJS.js.map
