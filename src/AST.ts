428112f4a1d77e17e0afd08b22ae99c3e55c83c9
dcalsky <dcalsky@163.com>
2017-09-23 18:40:47 +0800

    feat(AST): handle incompatible situation

--- src/AST.ts
+++ src/AST.ts
@@ -1,7 +1,9 @@
 import { transform as trans, transformFromAst } from 'babel-core'
 const walk = require('babylon-walk')
 import * as t from 'babel-types';
-import { get_property_mapping } from './Compare';
+import { get_property_mapping } from './Compare'
+import { output_log } from './Logger'
+const chalk = require('chalk')
 
 // {
 //   type: 'function_name',
@@ -26,30 +28,39 @@ import { get_property_mapping } from './Compare';
 // }
 
 const visitors = {
-  CallExpression(node, state, c) {
-    if (t.isIdentifier(node.callee)) {
-      const name = node.callee.name
-      node.callee.name = name === 'wx' ? 'my' : name
-    } else {
-      c(node.callee)
-    }
-  },
-  MemberExpression(node, state) {
+  // CallExpression(node, state, c) {
+  //   if (t.isIdentifier(node.callee)) {
+  //     const name = node.callee.name
+  //     node.callee.name = name === 'wx' ? 'my' : name
+  //   }
+  // },
+  MemberExpression(node, code) {
     const mappings = get_property_mapping()
     // Defaultly, this node is an identifier
     if (node.object.name === 'wx') {
-      node.object.name = 'my'
       mappings.forEach(mapping => {
-        if (mapping && mapping.from === node.property.name) {
+        if (!mapping.level && mapping.from === node.property.name) {
           node.property.name = mapping.to
+          node.object.name = 'my'
+          console.log(chalk.green((`Transform Correctly: ${node.object.name}.${node.property.name} Start: ${node.start}, end: ${node.end}\n\n\n`)))
+        } else if (mapping.level && mapping.key === node.property.name) {
+          console.log(chalk.red(code.slice(node.start - 50, node.end + 50)))
+          console.log(((`Transform Error: Start: ${node.start}, end: ${node.end}`)))
+          console.log(chalk.red('===================================================\n\n\n'))
         }
       })
     }
   }
 }
 
+function to_line_break(text: string, start: number, end: number, type = 1) {
+  if (type === 1) {
+
+  }
+}
+
 export function transform(code: string): string {
   const ast = trans(code).ast
-  const node = walk.recursive(ast, visitors, {})
+  const node = walk.recursive(ast, visitors, code)
   return transformFromAst(ast).code
 }
--- test/AST.js
+++ test/AST.js
@@ -4,18 +4,18 @@ const { transform } = require('../dist/AST.js')
 describe('AST', function() {
 
   describe('convert main module to ali style', function() {
-    it('convert wx() to my()', function() {
-      const code = `wx();`
-      transform(code)
-      // assert.equal(transform(code), `my();`);
-    });
+    // it('not convert wx() to my()', function() {
+    //   const code = `wx();`
+    //   transform(code)
+    //   assert.notEqual(transform(code), `my();`);
+    // });
 
-    it('convert wx variable in member relationship', function() {
-      const code_callee = `wx.a();`
-      const code_ident = `wx.a;`
-      assert.equal(transform(code_callee), `my.a();`)
-      assert.equal(transform(code_ident), `my.a;`)
-    });
+    // it('convert wx variable in member relationship', function() {
+    //   const code_callee = `wx.a();`
+    //   const code_ident = `wx.a;`
+    //   assert.equal(transform(code_callee), `my.a();`)
+    //   assert.equal(transform(code_ident), `my.a;`)
+    // });
     
     it('not convert wx variable when it as an arguments', function() {
       const code = `console.log(wx);`
@@ -24,7 +24,12 @@ describe('AST', function() {
 
     it('convert wx.getClipboardData to my.getClipboard', function() {
       const code = `wx.getClipboardData();`
-      assert.notEqual(transform(code), `my.getClipboard();`);
+      assert.equal(transform(code), `my.getClipboard();`);
+    });
+
+    it('not convert wx.stopCompass to my.stopCompass', function() {
+      const code = `wx.stopCompass();`
+      assert.notEqual(transform(code), `my.stopCompass();`);
     });
   });
 
